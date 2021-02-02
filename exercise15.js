ba5e7dddb0e3eac2afde986119ffdc9e69adc0d2
psiccardi <pietrosiccardi1989@gmail.com>
2021-02-02 11:51:48 +0100

    added exercise15

--- exercise15.js
+++ exercise15.js
@@ -1,22 +1,27 @@
 (function () {
     var euler = EulerFactory.getInstance();
     euler.addFunction(function (...args) {
-
-        // RIGHT = 1
         var numRight = args[0];
-        // DOWN = 0
         var numDown = args[1];
 
-        var total = numRight + numDown;
-        var startString = ''.padStart(numDown, '0') + ''.padStart(numRight, '1');
-        var endString = ''.padStart(numRight, '1') + ''.padStart(numDown, '0');
-        
-        var count = 2;
-        var startNum = parseInt(startString,2) + 1;
-        var endNum = parseInt(endString, 2) - 1;
-        for (let i=startNum; i<=endNum; i++) {
-            
+        var num = [];
+        var den = [];
+
+        // n!/k!(n-k)! (numRight+numDown)!/(numDown)!(numRight)!
+        for (var i = numRight + 1; i <= numRight + numDown; i++) {
+            num.push(i);
         }
-        return endString + '|' + startString;
-    }, 15, [2, 2])
+        for (var i = 2; i <= numDown; i++) {
+            den[i - 2] = i;
+            for (var j = 0; j < num.length; j++) {
+                if (num[j] % i == 0 && num[j] >= i) {
+                    num[j] /= i;
+                    den[i - 2] = 1;
+                    break;
+                }
+            }
+        }
+
+        return num.reduce((a, b) => a * b) / den.reduce((a, b) => a * b);
+    }, 15, [20, 20])
 }())
